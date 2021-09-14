42d7e2579ddea39b3bcacd73dd710a52b08073ff
Crowdbotics <team@crowdbotics.com>
2021-09-14 17:19:11 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "odd_heart_30538Identifier",
+  url: "https://odd_heart_30538Identifier.botics.co",
+  api: "https://odd_heart_30538Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
